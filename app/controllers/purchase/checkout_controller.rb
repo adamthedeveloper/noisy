3659099af5363f52849ddc8b498d16055bd55f63
Adam Medeiros <adammede@gmail.com>
2011-07-17 23:11:25 -0700

    Added code to get the wepay user

--- app/controllers/purchase/checkout_controller.rb
+++ app/controllers/purchase/checkout_controller.rb
@@ -3,7 +3,9 @@ class Purchase::CheckoutController < Purchase::PurchaseController
 
   def index
     if current_profile.has_wepay_auth_code?
-      wepay_auth_code = current_profile.wepay_auth_code
+      File.open('/tmp/wepay.log', 'a') {|f| f.write("Inside index #{current_profile.wepay_auth_code}") }
+      gateway.wepay_auth_code = current_profile.wepay_auth_code
+      File.open('/tmp/wepay.log', 'a') {|f| f.write("Inside index, gateway.wepay_auth_code is #{gateway.wepay_auth_code}") }
       render :json => gateway.wepay_user
     else
       scope = ['refund_payments','collect_payments','view_balance','view_user']
