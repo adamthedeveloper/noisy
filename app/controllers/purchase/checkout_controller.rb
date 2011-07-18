65dc3aba13025c0a82c1f0e00d2bfc2100277b1b
Adam Medeiros <adammede@gmail.com>
2011-07-17 21:09:29 -0700

    Added code to get the wepay user

--- app/controllers/purchase/checkout_controller.rb
+++ app/controllers/purchase/checkout_controller.rb
@@ -3,8 +3,8 @@ class Purchase::CheckoutController < Purchase::PurchaseController
 
   def index
     if current_profile.has_wepay_auth_code?
-      wepay_auth_code=(current_profile.wepay_auth_code)
-      render :json => wepay_user
+      gateway.wepay_auth_code = current_profile.wepay_auth_code
+      render :json => gateway.wepay_user
     else
       scope = ['refund_payments','collect_payments','view_balance','view_user']
       redirect_to_wepay_for_auth(wepay_authorize_index_url,scope)
