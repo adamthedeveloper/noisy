6a86bd0b64aac4270eb876c7d74b0d3ba86211b5
Adam Medeiros <adammede@gmail.com>
2011-07-20 22:59:41 -0700

    Added code to get the wepay user

--- app/controllers/purchase/checkout_controller.rb
+++ app/controllers/purchase/checkout_controller.rb
@@ -4,16 +4,12 @@ class Purchase::CheckoutController < Purchase::PurchaseController
   def index
     if current_profile.has_wepay_auth_code?
       if wepay_access_token_exists?
-        logger.debug "CheckoutController#index if wepay_access_token_exists?"
       else
-        logger.debug "CheckoutController#index else"
         initialize_wepay_access_token(current_profile.wepay_auth_code)
-        logger.debug "CheckoutController#index after initialize_wepay_access_token call - sending #{current_profile.wepay_auth_code}"
       end
       render :json => gateway.wepay_user
     else
-      scope = ['refund_payments','collect_payments','view_balance','view_user']
-      redirect_to_wepay_for_auth(scope)
+      redirect_to_wepay_for_auth
     end
   end
 end
