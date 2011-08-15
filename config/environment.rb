1a413c3c516ef37a398a9616a0f8d93ba2b72233
Adam Medeiros <adammede@gmail.com>
2011-08-14 22:28:09 -0700

    UTF-8 fix?

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Noisebytes::Application.initialize!
+
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
